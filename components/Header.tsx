7a144b17c80f3546426f64b6852e7f5c7931b06c
chadmuro <chadmuro@gmail.com>
2021-07-04 18:49:56 +0900

    set up tabs to show different views

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export default function Header() {
+	const router = useRouter();
+	const [view, setView] = useState<string>(router.pathname);
+
 	return (
 		<>
-			<header className="h-20 w-full max-w-2xl flex justify-between items-center">
+			<header className="h-20 w-full max-w-4xl flex justify-between items-center">
 				<h2 className="text-xl">Gaijin Dakara</h2>
 				<div>
 					<button className="bg-red-300 hover:bg-red-500 text-black px-2 py-1 rounded outline-none focus:outline-none focus:ring-2 focus:ring-white mr-3">
@@ -16,6 +23,28 @@ export default function Header() {
 				<h3 className="text-xl text-center italic">
 					Helping foreigners understand the unwritten rules of Japan.
 				</h3>
+			</div>
+			<div className="mt-4">
+				<ul className="flex">
+					<li className="mx-4" onClick={() => setView('/random')}>
+						<Link href="/random">Random</Link>
+						{view === '/' && <div className="h-1 w-full bg-red-300" />}
+						{view === '/random' && <div className="h-1 w-full bg-red-300" />}
+					</li>
+
+					<li className="mx-4" onClick={() => setView('/categories')}>
+						<Link href="/categories">Categories</Link>
+						{view === '/categories' && (
+							<div className="h-1 w-full bg-red-300" />
+						)}
+					</li>
+					<li className="mx-4" onClick={() => setView('/contribute')}>
+						<Link href="/contribute">Contribute</Link>
+						{view === '/contribute' && (
+							<div className="h-1 w-full bg-red-300" />
+						)}
+					</li>
+				</ul>
 			</div>
 		</>
 	);
